5f254646b16ee5e8b9f9b8f108c3964c459ce6b8
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-12-10 00:12:08 +0900

    Delete Console

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -8,7 +8,6 @@ window.addEventListener('load', function () { // イベント
     let elmLength = setElm.length, // 画像数
       randomSet = Math.floor(Math.random() * elmLength); // ランダム数
 
-    console.log(setElm[randomSet]);
     setElm[randomSet].style.display = 'block'; // CSS指定
     setElm[randomSet].animate([{ opacity: 0 }, { opacity: 1 }], fadeSpeed); // フェードイン
     setElm = Array.from(setElm); // 配列に変換
