831c2c2c79f695f0b7b49bed2b9c29b415215cc8
Yuuu <86790336+muffin-bird@users.noreply.github.com>
2022-12-14 02:37:43 +0900

    Layout Fixed

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,6 +1,6 @@
-let setElm = document.querySelectorAll('#boxList .img'),
-  delaySpeed = 1000,
-  fadeSpeed = 1500;
+let setElm = document.querySelectorAll('#boxList .box'),
+  delaySpeed = 600,
+  fadeSpeed = 1100;
 
 window.addEventListener('load', function () { // イベント
   randomShow();
@@ -8,7 +8,7 @@ window.addEventListener('load', function () { // イベント
     let elmLength = setElm.length, // 画像数
       randomSet = Math.floor(Math.random() * elmLength); // ランダム数
 
-    setElm[randomSet].style.display = 'block'; // CSS指定
+    setElm[randomSet].style.visibility = 'visible'; // CSS指定
     setElm[randomSet].animate([{ opacity: 0 }, { opacity: 1 }], fadeSpeed); // フェードイン
     setElm = Array.from(setElm); // 配列に変換
     setElm.splice(randomSet, 1); // 要素の取り除き
