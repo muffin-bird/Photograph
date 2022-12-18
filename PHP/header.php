6f53ad761a8371adcef057edf96c68d4aba370d1
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-12-19 03:29:40 +0900

    Test Template tag

--- PHP/header.php
+++ PHP/header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <link rel="stylesheet" href="./assets/stylesheets/css/destyle.css">
-    <link rel="stylesheet" href="./assets/stylesheets/css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/stylesheets/css/destyle.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>//assets/stylesheets/css/style.css">
     <title>name</title>
     <?php wp_head(); ?>
 </head>
