82cc5d34d9afa7448f4b53356cdef64d3f4886bf
Yuuu <86790336+muffin-bird@users.noreply.github.com>
2022-12-23 18:22:35 +0900

    Add post_has_archive()

---
+++ PHP/archive-photography.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+    <div class="container">
+        <section class="section_secondary">
+            <div id="boxList">
+                <ul class="boxes">
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                    <li class="box">
+                        <a href="single-photography.html" class="link">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/laundry.jpg" alt="thumbnail" class="img">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
+<?php get_footer(); ?>
