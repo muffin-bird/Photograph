63bcd4e4e850d77af3e5c2bb129a8aba2cca5c15
Yuuu <86790336+muffin-bird@users.noreply.github.com>
2022-12-25 04:38:00 +0900

    Add have_posts()

--- PHP/archive.php
+++ PHP/archive.php
@@ -2,8 +2,10 @@
     <div class="container">
         <section class="section_secondary">
             <div class="boxes">
+                <!-- ループ開始 -->
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article class="card">
-                    <a href="single-news.html" class="link">
+                    <a href="<?php the_permalink(); ?>" class="link">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/news.jpg" alt="thumbnail" class="img">
                         <div class="meta">
                             <time class="date">2022.11.19</time>
@@ -12,26 +14,8 @@
                         </div>
                     </a>
                 </article>
-                <article class="card">
-                    <a href="single-news.html" class="link">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/news.jpg" alt="thumbnail" class="img">
-                        <div class="meta">
-                            <time class="date">2022.11.19</time>
-                            <h2 class="title">ホームページ開設</h2>
-                            <p class="txt">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが...</p>
-                        </div>
-                    </a>
-                </article>
-                <article class="card">
-                    <a href="single-news.html" class="link">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/news.jpg" alt="thumbnail" class="img">
-                        <div class="meta">
-                            <time class="date">2022.11.19</time>
-                            <h2 class="title">ホームページ開設</h2>
-                            <p class="txt">ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが...</p>
-                        </div>
-                    </a>
-                </article>
+                <?php endwhile; endif; ?>
+                <!-- ループ終わり -->
             </div>
         </section>
     </div>
