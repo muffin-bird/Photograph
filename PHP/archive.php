d213d53fee46a7c67a9852c0489f191100ff7777
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-12-27 18:17:59 +0900

    Change Code Single.php

--- PHP/archive.php
+++ PHP/archive.php
@@ -6,11 +6,11 @@
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article class="card">
                     <a href="<?php the_permalink(); ?>" class="link">
-                        <span class="thumbnail"><?php the_post_thumbnail(); ?></span>
+                        <div class="thumbnail"><?php the_post_thumbnail(); ?></div>
                         <div class="meta">
                             <time class="date" datetime="<?php the_time('Y.m.d'); ?>"><?php the_time('Y.m.d'); ?></time>
                             <h2 class="title"><?php the_title(); ?></h2>
-                            <p class="txt"><?php the_content(); ?></p>
+                            <p class="txt"><?php the_excerpt(); ?></p>
                         </div>
                     </a>
                 </article>
--- PHP/single.php
+++ PHP/single.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?>
     <div class="container">
         <section class="section">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article class="post">
                 <div class="headarea">
-                    <h2 class="heading">ホームページ開設</h2>
-                    <time class="date">2022.11.19</time>
+                    <h2 class="heading"><?php the_title(); ?></h2>
+                    <time class="date" datetime="<?php the_time('Y.m.d'); ?>"><?php the_time('Y.m.d'); ?></time>
                 </div>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/news.jpg" alt="thumbnail" class="img">
-                <p class="text">
-                    ここにテキストが入ります。ここにテキストが入ります。<br>
-                    ここにテキストが入ります。ここにテキストが入ります。<br>
-                    ここにテキストが入ります。ここにテキストが入ります。<br>
-                </p>
+                <div class="thumbnail single"><?php the_post_thumbnail(); ?></div>
+                <p class="text"><?php the_content(); ?></p>
             </article>
+            <?php endwhile; endif; ?>
         </section>
     </div>
 <?php get_footer(); ?>
